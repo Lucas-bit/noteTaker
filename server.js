ca52f4ef3e98badc7a85ad2695a81f581384d45c
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2020-02-03 22:46:20 -0500

    have the notes saving but need to update delete request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 
+const storage = require("./db/storage")
+
 const noteTitle = [];
 const noteText = [];
 
@@ -13,17 +15,52 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //turns data into json object
 
+
+//==========================
+// Routes
+//==========================
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
+  app.get("/assets/js/index.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
+  });
+
+  app.get("/assets/css/styles.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
   });
 
   app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
-  
+  app.get("/api/notes", (req, res) => {
+    storage.getNotes()
+    .then(notes => res.json(notes))
+    .catch(error => res.status(500).json(error)) 
+  });
 
 
+
+  app.get("/api/noteText", (req, res) => {
+    res.json(noteText)
+  });
+
+  app.post("/api/notes",(req, res) => {
+    storage.AddNotes(req.body)
+    .then((note) => res.json(note))
+    .catch(error => res.status(500).json(error)) 
+  })
+
+  app.delete("/api/notes/:id", (req, res) => {
+    storage.removeNote(req.param.id)
+    .then(() => res.json({
+      ok: true //yes we deleted the note succesfully
+    }))
+    .catch(error => res.status(500).json(error)) 
+
+  })
 
 //=====================================================
   // Server listen
