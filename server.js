2ec5cf07489a2179e49ccc445f1dc4cf7af46cf4
Lucas-bit <lucasbertaudgonzalez@gmail.com>
2020-04-03 16:26:44 -0400

    rearranged organization of files and incorporated uuidv1 to dynamically create unique ids

--- server.js
+++ server.js
@@ -1,80 +1,17 @@
 const express = require("express");
-const path = require("path");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-const storage = require("./db/storage")
-
-const noteTitle = [];
-const noteText = [];
-
-
-//Sets uo Express app
+// Initialize the app and create a port
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
+// Set up body parsing, static, and route middleware
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); //turns data into json object
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-
-//==========================
-// Routes
-//==========================
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
-
-  app.get("/assets/js/index.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
-  });
-
-  app.get("/assets/css/styles.css", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
-  });
-
-  app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
-
-  app.get("/api/notes", (req, res) => {
-    storage.getNotes()
-    .then(notes => res.json(notes))
-    .catch(error => res.status(500).json(error)) 
-  });
-
-
-
-
-
-  app.get("/api/noteText", (req, res) => {
-    res.json(noteText)
-  });
-
-  app.post("/api/notes",(req, res) => {
-    storage.AddNotes(req.body)
-    .then((note) => res.json(note))
-    .catch(error => res.status(500).json(error)) 
-  })
-
-  app.delete("/api/notes/:id", (req, res) => {
-    
-    storage.removeNote(req.param.id)
-    
-    console.log(storage.removeNote(req.param.id))
-    
-    .then(() => res.json({
-      ok: true //yes we deleted the note succesfully
-    }))
-    .catch(error => res.status(500).json(error)) 
-      
-  });
-
-//=====================================================
-  // Server listen
-//=====================================================
-
-
-app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
-  });
-
-  
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
